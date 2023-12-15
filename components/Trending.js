84a41364ebddebb48c3828cebda8b9f6bdbe8b0c
Maurice <mauriceglenn2@gmail.com>
2023-12-15 13:33:01 -0600

    add photos to who to follow

--- components/Trending.js
+++ components/Trending.js
@@ -39,11 +39,10 @@ export default function Trending() {
         </div>
         <div className="relative p-3">
           <DotsHorizontalIcon className="w-5 text-gray-600 absolute right-4" />
-          <p className="text-xs text-gray-500">Trending in US</p>
-          <h1 className="text-[15px] font-bold">China</h1>
+          <p className="text-xs text-gray-500">NBA</p>
+          <h1 className="text-[15px] font-bold">Warriors at Clippers</h1>
           <p className="text-xs text-gray-500">1,000 Tweets</p>
         </div>
-        
       </div>
       <div className="w-[300px] h-[300px] bg-white bg-opacity-10 rounded-3xl mt-3 text-white">
         <h1 className="font-bold text-xl p-3">Who to Follow</h1>
@@ -68,15 +67,15 @@ export default function Trending() {
         <div className="flex justify-between p-3">
           <div className="flex space-x-3 ">
             <img
-              src="/assets/Me pic.jpeg"
+              src="/assets/sukuna.jpg"
               className="h-11 w-11 rounded-full object-cover"
             />
             <div>
               <div className="flex space-x-1">
-                <h1 className="font-bold text-lg">Moe</h1>
+                <h1 className="font-bold text-lg">Sukuna</h1>
                 <BadgeCheckIcon className="w-[18px] text-blue-400" />
               </div>
-              <h1 className="text-[12px] text-gray-500 mt-1">@Moe</h1>
+              <h1 className="text-[12px] text-gray-500 mt-1">@Sukuna</h1>
             </div>
           </div>
           <button className="bg-white text-black text-sm w-20 h-8 font-bold rounded-3xl">
@@ -86,15 +85,15 @@ export default function Trending() {
         <div className="flex justify-between p-3">
           <div className="flex space-x-3 ">
             <img
-              src="/assets/Me pic.jpeg"
+              src="/assets/luffy.avif"
               className="h-11 w-11 rounded-full object-cover"
             />
             <div>
               <div className="flex space-x-1">
-                <h1 className="font-bold text-lg">Moe</h1>
+                <h1 className="font-bold text-lg">Luffy</h1>
                 <BadgeCheckIcon className="w-[18px] text-blue-400" />
               </div>
-              <h1 className="text-[12px] text-gray-500 mt-1">@Moe</h1>
+              <h1 className="text-[12px] text-gray-500 mt-1">@Luffy</h1>
             </div>
           </div>
           <button className="bg-white text-black text-sm w-20 h-8 font-bold rounded-3xl">
